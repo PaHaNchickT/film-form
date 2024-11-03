46d53a9cf456d6c223ee3913dc1e6ab3985e63b5
Pavel <you@example.com>
2024-11-03 23:01:47 +0300

    feat: implement validation for all fields

--- src/validation/FormSchema.ts
+++ src/validation/FormSchema.ts
@@ -3,12 +3,18 @@ import { z } from 'zod';
 
 import { TEXT_CONTENT } from '@/constants/const-text-content';
 
-const QuestionFormSchema = (): ZodSchema => {
+const FormSchema = (): ZodSchema => {
   const schema = z.object({
-    answer: z.any().refine((value) => value.length, { message: TEXT_CONTENT.questions.errorText }),
+    title: z.string().min(1, TEXT_CONTENT.error.validationRequired),
+    genre: z.string().min(1, TEXT_CONTENT.error.validationRequired),
+    format: z.string().min(1, TEXT_CONTENT.error.validationRequired),
+    unf: z.string().regex(/^(\d{3}-\d{3}-\d{3}-\d{2}-\d{3}|)$/, TEXT_CONTENT.error.validationFormat),
+    country: z.string().min(1, TEXT_CONTENT.error.validationRequired),
+    price: z.any().refine((value) => !isNaN(+value), { message: TEXT_CONTENT.error.validationNumber }),
+    synopsis: z.string(),
   });
 
   return schema;
 };
 
-export default QuestionFormSchema;
+export default FormSchema;
